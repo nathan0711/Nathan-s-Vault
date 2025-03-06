3bef70d37bc0454ccd7df1c1390179d318a6d393
NathanDoan <doanquan199916@gmail.com>
2025-03-05 16:07:54 -0800

    nathan flie

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// message
+const message = "You have received this message because you have been chosen to open an important vault";
+//num1 = 10
+const num1 = 9 + 1;
+//num2 = 40
+const num2 = 60-20;
+//num3 = 39
+const num3 = 13*3 ;
+
+//
+alert(`${message}: ${num1}- ${num2}- ${num3}`)
